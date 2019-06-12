972bcf1854caf21ddccfef4e34c67dc8537e3305
Dimazsever <40835695+Dimazsever@users.noreply.github.com>
2019-06-13 01:06:05 +0300

    Update scene.js

--- scene.js
+++ scene.js
@@ -13,7 +13,7 @@ var createScene = function() {
     var light = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, 1, 0), scene);
     light.intensity = 0.7;
     light.diffuse = new BABYLON.Color3(1, 0, 0);
-    BABYLON.SceneLoader.Append("https://dimazsever.github.io/edward/", "parallel_bars.obj", scene);
+    BABYLON.SceneLoader.Append("", "parallel_bars.obj", scene);
     scene.createDefaultCamera(true, true, true);
 
     
