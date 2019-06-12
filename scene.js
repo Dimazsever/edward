9641a56bb96962afc63e5c9ca693d809eafe050f
Dimazsever <40835695+Dimazsever@users.noreply.github.com>
2019-06-13 01:04:14 +0300

    Update scene.js

--- scene.js
+++ scene.js
@@ -6,34 +6,19 @@ canvas.height = "680";
 // Load the 3D engine
 var engine = new BABYLON.Engine(canvas, true, {preserveDrawingBuffer: true, stencil: true});
 // CreateScene function that creates and return the scene
-var createScene = function(){
-    // Create a basic BJS Scene object
-    var scene = new BABYLON.Scene(engine);
-	// loader
-    var loader = new BABYLON.AssetsManager(scene);
-    // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
-    // Target the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
-    // Attach the camera to the canvas
-    camera.attachControl(canvas, false);
-	//box
-	var box = loader.addMeshTask("box", "", "", "parallel_bars.obj");
+var createScene = function() {
+	var scene = new BABYLON.Scene(engine);
 	
-    // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
-    var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
-	light.intensity = 0.8;
-    // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
-    //var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
-    // Move the sphere upward 1/2 of its height
-    //sphere.position.y = 1;
-    // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
-    var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
-	
-	loader.load();
-    // Return the created scene
+    //var box = BABYLON.MeshBuilder.CreateBox("box", {});
+    var light = new BABYLON.HemisphericLight("Hemi0", new BABYLON.Vector3(0, 1, 0), scene);
+    light.intensity = 0.7;
+    light.diffuse = new BABYLON.Color3(1, 0, 0);
+    BABYLON.SceneLoader.Append("https://dimazsever.github.io/edward/", "parallel_bars.obj", scene);
+    scene.createDefaultCamera(true, true, true);
+
+    
     return scene;
-}
+};
 // call the createScene function
 var scene = createScene();
 // run the render loop
