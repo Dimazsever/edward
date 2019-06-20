c406d54ef83c1f200818df69f6b3bc2e5b8dee61
Dimazsever <40835695+Dimazsever@users.noreply.github.com>
2019-06-21 00:01:45 +0300

    Update scene.js

--- scene.js
+++ scene.js
@@ -10,11 +10,11 @@ var createScene = function(){
     // Create a basic BJS Scene object
     var scene = new BABYLON.Scene(engine);
     // Create a FreeCamera, and set its position to {x: 0, y: 5, z: -10}
-    var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
+    //var camera = new BABYLON.FreeCamera('camera1', new BABYLON.Vector3(0, 5, -10), scene);
     // Target the camera to scene origin
-    camera.setTarget(BABYLON.Vector3.Zero());
+    //camera.setTarget(BABYLON.Vector3.Zero());
     // Attach the camera to the canvas
-    camera.attachControl(canvas, false);
+    //camera.attachControl(canvas, false);
     // Create a basic light, aiming 0, 1, 0 - meaning, to the sky
     var light = new BABYLON.HemisphericLight('light1', new BABYLON.Vector3(0, 1, 0), scene);
     // Create a built-in "sphere" shape; its constructor takes 6 params: name, segment, diameter, scene, updatable, sideOrientation
@@ -22,7 +22,9 @@ var createScene = function(){
     //var sphere = BABYLON.Mesh.CreateSphere('sphere1', 16, 2, scene, false, BABYLON.Mesh.FRONTSIDE);
     // Move the sphere upward 1/2 of its height
     //sphere.position.y = 1;
-    BABYLON.SceneLoader.Append("", "parallel_bars.obj", scene);
+    BABYLON.SceneLoader.Append("", "parallel_bars.obj", scene, function(scene){
+        scene.createDefaultCamera(true, true, true);
+    });
     // Create a built-in "ground" shape; its constructor takes 6 params : name, width, height, subdivision, scene, updatable
     var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
     // Return the created scene
