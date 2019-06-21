fa6ae12c879138305ab62524a877d3cf6f37c4d7
Dimazsever <40835695+Dimazsever@users.noreply.github.com>
2019-06-21 17:39:37 +0300

    Update scene.js

--- scene.js
+++ scene.js
@@ -21,11 +21,12 @@ var createScene = function(){
     //BABYLON.SceneLoader.Append("", "parallel_bars.obj", scene, function(scene){
     BABYLON.SceneLoader.ImportMesh("", "", "parallel_bars.obj", scene, function (newMeshes) {
         var bars = newMeshes[0];
+        bars.physicsImpostor = new BABYLON.PhysicsImpostor(bars, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
         var angle = 0.01;
-    var earthAxis = new BABYLON.Vector3(Math.sin(0 * Math.PI/180), Math.cos(0 * Math.PI/180), 0);
+    /*var earthAxis = new BABYLON.Vector3(Math.sin(0 * Math.PI/180), Math.cos(0 * Math.PI/180), 0);
     scene.registerBeforeRender(function() {
         bars.rotate(earthAxis, angle, BABYLON.Space.WORLD);
-    })
+    })*/
         scene.createDefaultCamera(true, true, true);
         // run the render loop
         engine.runRenderLoop(function(){
@@ -37,7 +38,7 @@ var createScene = function(){
     var ground = BABYLON.Mesh.CreateGround('ground1', 6, 6, 2, scene, false);
     
     scene.enablePhysics();
-    bars.physicsImpostor = new BABYLON.PhysicsImpostor(bars, BABYLON.PhysicsImpostor.SphereImpostor, { mass: 1, restitution: 0.9 }, scene);
+    
     ground.physicsImpostor = new BABYLON.PhysicsImpostor(ground, BABYLON.PhysicsImpostor.BoxImpostor, { mass: 0, restitution: 0.9 }, scene);
     // Return the created scene
     return scene;
